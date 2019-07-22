57195e2185e4a3feb20618727a25d483adc6258b
naiemk <naiemk@gmail.com>
2019-07-22 00:59:58 -0400

    Fix typo for network name

--- models/types/AccountTypes.ts
+++ models/types/AccountTypes.ts
@@ -1,6 +1,6 @@
 
 export const API_VERSION = '0.0.1';
 
-export type Network = 'ETHEREUM' | 'BITCOIN' | 'BINACNCE' | 'FERRUM';
+export type Network = 'ETHEREUM' | 'BITCOIN' | 'BINANCE' | 'FERRUM';
 
 export type AccountTransactionHandlerService = 'kudi' | 'unifyre' | 'netswap';
