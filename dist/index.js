d0a569bb44d426534f1ade94e22a863e60b14b80
naiemk <naiemk@gmail.com>
2019-07-22 17:58:22 -0400

    Fix typo

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,5 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./types/AccountTypes"));
+__export(require("./aws/client/JsonRpcClient"));
 //# sourceMappingURL=index.js.map
--- models/index.ts
+++ models/index.ts
@@ -1,5 +1,6 @@
 
 export * from './types/AccountTypes';
 export * from './aws/messages/AccountMessages';
+export * from './aws/client/JsonRpcClient';
 export * from './data/AccountData';
-export * from './data/CruptoData';
+export * from './data/CryptoData';
