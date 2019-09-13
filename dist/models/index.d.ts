e6279555cac6d1633cda3aebd37864f2a86cb244
Naiem Yeganeh <naiemk@gmail.com>
2019-09-12 20:22:34 -0400

    Adding crypto types to the index

--- dist/models/index.d.ts
+++ dist/models/index.d.ts
@@ -3,5 +3,6 @@ export * from './aws/messages/AccountMessages';
 export * from './aws/client/JsonRpcClient';
 export * from './data/AccountData';
 export * from './data/CryptoData';
+export * from './data/Types';
 export * from '../ioc/Container';
 //# sourceMappingURL=index.d.ts.map
--- models/index.ts
+++ models/index.ts
@@ -4,5 +4,6 @@ export * from './aws/messages/AccountMessages';
 export * from './aws/client/JsonRpcClient';
 export * from './data/AccountData';
 export * from './data/CryptoData';
+export * from './data/Types';
 
 export * from '../ioc/Container';
