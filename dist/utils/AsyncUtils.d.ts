1ac8f8e65c4ca13aefbf0da3b349d5ea31426453
Naiem Yeganeh <naiem@ferrumnet.org>
2019-10-28 00:37:54 -0400

    Add retry

--- dist/utils/AsyncUtils.d.ts
+++ dist/utils/AsyncUtils.d.ts
@@ -1,2 +1,9 @@
+export declare const globalRetryConfig: {
+    defaultTimeout: number;
+    count: number;
+};
+export declare class RetryableError extends Error {
+}
 export declare function sleep(ms: number): Promise<unknown>;
+export declare function retry<T>(fun: () => Promise<T>): Promise<T | undefined>;
 //# sourceMappingURL=AsyncUtils.d.ts.map
--- utils/AsyncUtils.ts
+++ utils/AsyncUtils.ts
@@ -1,4 +1,23 @@
+export const globalRetryConfig = {
+  defaultTimeout: 300,
+  count: 3,
+};
+
+export class RetryableError extends Error {}
 
 export function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function retry<T>(fun: () => Promise<T>) {
+  for(let i = 0; i < globalRetryConfig.count; i++) {
+    try {
+      return await fun();
+    } catch (e) {
+      if (e instanceof RetryableError) {
+        // pass
+        await sleep(globalRetryConfig.defaultTimeout * Math.pow(2, i));
+      } else throw e;
+    }
+  }
 }
