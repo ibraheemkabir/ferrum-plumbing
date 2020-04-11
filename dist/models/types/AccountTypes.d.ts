0e9abdca508ca18618697404367fb10565c793f2
Naiem Yeganeh <naiem@ferrumnet.org>
2020-04-10 23:32:51 -0400

    Add BITCOIN_TESTNET to Networks

--- dist/models/types/AccountTypes.d.ts
+++ dist/models/types/AccountTypes.d.ts
@@ -1,4 +1,4 @@
 export declare const API_VERSION = "0.0.1";
-export declare type Network = 'ETHEREUM' | 'BITCOIN' | 'BINANCE' | 'BINANCE_TESTNET' | 'FERRUM' | 'RINKEBY';
+export declare type Network = 'ETHEREUM' | 'BITCOIN' | 'BITCOIN_TESTNET' | 'BINANCE' | 'BINANCE_TESTNET' | 'FERRUM' | 'RINKEBY';
 export declare type AccountTransactionHandlerService = 'kudi' | 'unifyre' | 'netswap';
 //# sourceMappingURL=AccountTypes.d.ts.map
--- models/types/AccountTypes.ts
+++ models/types/AccountTypes.ts
@@ -1,6 +1,6 @@
 
 export const API_VERSION = '0.0.1';
 
-export type Network = 'ETHEREUM' | 'BITCOIN' | 'BINANCE' | 'BINANCE_TESTNET' | 'FERRUM' | 'RINKEBY';
+export type Network = 'ETHEREUM' | 'BITCOIN' | 'BITCOIN_TESTNET' | 'BINANCE' | 'BINANCE_TESTNET' | 'FERRUM' | 'RINKEBY';
 
 export type AccountTransactionHandlerService = 'kudi' | 'unifyre' | 'netswap';
