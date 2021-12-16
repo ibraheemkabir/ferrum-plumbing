f1b82d470868044c36f54fa83f024c96b64b4361
mahmedsaeediferrum <ahmed.saeedi@ferrum.network>
2021-12-16 23:17:04 +0500

    added avax and moonriver

--- dist/models/types/AccountTypes.d.ts
+++ dist/models/types/AccountTypes.d.ts
@@ -1,5 +1,5 @@
 export declare const API_VERSION = "0.0.1";
-export declare type Network = 'ETHEREUM' | 'BITCOIN' | 'BITCOIN_TESTNET' | 'BINANCE' | 'BINANCE_TESTNET' | 'FERRUM' | 'RINKEBY' | 'BSC' | 'BSC_TESTNET' | 'POLYGON' | 'MUMBAI_TESTNET' | 'SOLANA' | 'SOLANA_TESTNET' | 'AVAX' | 'AVAX_TESTNET';
+export declare type Network = 'ETHEREUM' | 'BITCOIN' | 'BITCOIN_TESTNET' | 'BINANCE' | 'BINANCE_TESTNET' | 'FERRUM' | 'RINKEBY' | 'BSC' | 'BSC_TESTNET' | 'POLYGON' | 'MUMBAI_TESTNET' | 'SOLANA' | 'SOLANA_TESTNET' | 'AVAX' | 'AVAX_TESTNET' | 'AVAX_MAINNET' | 'MOON_MOONRIVER';
 export declare type AccountTransactionHandlerService = 'kudi' | 'unifyre';
 export interface NetworkedConfig<T> {
     [network: string]: T;
--- models/types/AccountTypes.ts
+++ models/types/AccountTypes.ts
@@ -4,7 +4,7 @@ export const API_VERSION = '0.0.1';
 export type Network = 'ETHEREUM' |
     'BITCOIN' | 'BITCOIN_TESTNET' | 'BINANCE' | 'BINANCE_TESTNET' | 'FERRUM' |
     'RINKEBY' | 'BSC' | 'BSC_TESTNET' | 'POLYGON' | 'MUMBAI_TESTNET' | 'SOLANA'
-    | 'SOLANA_TESTNET' | 'AVAX' | 'AVAX_TESTNET';
+    | 'SOLANA_TESTNET' | 'AVAX' | 'AVAX_TESTNET'|'AVAX_MAINNET'|'MOON_MOONRIVER';
 
 export type AccountTransactionHandlerService = 'kudi' | 'unifyre';
 
