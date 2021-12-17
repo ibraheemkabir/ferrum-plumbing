fec1b1088dcde0886ab87097b69e3b6f68f1395a
Naiem Yeganeh <naiem@Naiems-MacBook-Pro.local>
2021-12-17 08:14:54 -0700

    Add some more chains

--- models/types/AccountTypes.ts
+++ models/types/AccountTypes.ts
@@ -4,7 +4,9 @@ export const API_VERSION = '0.0.1';
 export type Network = 'ETHEREUM' |
     'BITCOIN' | 'BITCOIN_TESTNET' | 'BINANCE' | 'BINANCE_TESTNET' | 'FERRUM' |
     'RINKEBY' | 'BSC' | 'BSC_TESTNET' | 'POLYGON' | 'MUMBAI_TESTNET' | 'SOLANA'
-    | 'SOLANA_TESTNET' | 'AVAX' | 'AVAX_TESTNET'|'AVAX_MAINNET'|'MOON_MOONRIVER';
+    | 'SOLANA_TESTNET' | 'AVAX_TESTNET'|'AVAX_MAINNET'|
+    'MOON_MOONRIVER' | 'MOON_MOONBASE' | 'HARMONY_TESTNET' | 'HARMONY_MAINNET'
+    ;
 
 export type AccountTransactionHandlerService = 'kudi' | 'unifyre';
 
